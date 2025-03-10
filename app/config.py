94d495a4b4a2aba154e37c62155f8016ecf914e1
sagaat <a.silvestre@lyon.ort.asso.fr>
2025-03-10 16:05:12 +0100

    config add mongo-uri prod

--- app/config.py
+++ app/config.py
@@ -1,2 +1,3 @@
-MONGO_URI = "mongodb://host.docker.internal:27017/"
+#MONGO_URI = "mongodb://host.docker.internal:27017/"
+MONGO_URI = "mongodb://mongodb-0.mongodb-service:27017,mongodb-1.mongodb-service:27017,mongodb-2.mongodb-service:27017/TripShare?replicaSet=rs0&authSource=admin"
 DB_NAME = "TripShare"
